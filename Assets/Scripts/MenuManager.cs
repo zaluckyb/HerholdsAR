b6cca4347a99a1c86265be3ddfd85db3cd5217ed
Gerald Ferreira <zaluckyb@gmail.com>
2025-03-09 16:57:57 +0200

    update

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,65 +1,71 @@
 using UnityEngine;
 using Oculus.Interaction;
-using Oculus.Interaction.UnityCanvas;
-using UnityEngine.Events;
 using System.Collections.Generic;
 
-public class MenuManager : MonoBehaviour
+public class PokeMenuManager : MonoBehaviour
 {
     [System.Serializable]
-    public class InteractableMenuPair
+    public class PokeInteractableMenuPair
     {
-        public PokeInteractable interactable;
-        public GameObject menu;
+        public PokeInteractable pokeInteractable;  // Assign your interactable button here
+        public GameObject menu;                    // Corresponding menu to activate
     }
 
-    [Header("Interactable-Menu Pairs")]
-    public List<InteractableMenuPair> interactableMenuPairs;
+    [Header("Poke Interactable - Menu Pairs")]
+    public List<PokeInteractableMenuPair> pokeMenuPairs;
 
-    void Start()
+    private void Start()
     {
         ResetAllMenus();
 
-        // Assign event listeners dynamically
-        foreach (var pair in interactableMenuPairs)
+        foreach (var pair in pokeMenuPairs)
         {
-            var wrapper = pair.interactable.GetComponent<InteractableUnityEventWrapper>();
-            if (wrapper == null)
-            {
-                wrapper = pair.interactable.gameObject.AddComponent<InteractableUnityEventWrapper>();
-            }
-
-            wrapper.WhenSelect.AddListener(() => ActivateMenu(pair));
+            var localPair = pair; // Avoid closure issues
+            localPair.pokeInteractable.WhenStateChanged += args => HandlePoke(localPair, args);
         }
     }
 
-    void ActivateMenu(InteractableMenuPair activePair)
+    private void OnDestroy()
     {
-        foreach (var pair in interactableMenuPairs)
+        foreach (var pair in pokeMenuPairs)
+        {
+            if (pair.pokeInteractable != null)
+                pair.pokeInteractable.WhenStateChanged -= args => HandlePoke(pair, args);
+        }
+    }
+
+    private void HandlePoke(PokeInteractableMenuPair pair, InteractableStateChangeArgs args)
+    {
+        if (args.NewState == InteractableState.Select)
+        {
+            ActivateMenu(pair);
+        }
+    }
+
+    private void ActivateMenu(PokeInteractableMenuPair activePair)
+    {
+        foreach (var pair in pokeMenuPairs)
         {
             bool isActive = pair == activePair;
 
-            // Enable/Disable Interactable component
-            pair.interactable.enabled = !isActive;
-
-            // Enable/Disable corresponding menu
             if (pair.menu != null)
                 pair.menu.SetActive(isActive);
+
+            pair.pokeInteractable.enabled = !isActive;
         }
     }
 
     public void ResetAllMenus()
     {
-        foreach (var pair in interactableMenuPairs)
+        foreach (var pair in pokeMenuPairs)
         {
             if (pair.menu != null)
                 pair.menu.SetActive(false);
 
-            pair.interactable.enabled = true;
+            pair.pokeInteractable.enabled = true;
         }
     }
 
-    // For resetting via a specific interactable
     public void OnResetButtonClicked()
     {
         ResetAllMenus();
